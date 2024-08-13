7fbb650f4fcd57ccdd437045e88baaf6e0f7914d
Ishaan Jain <143131679+ishaanj26@users.noreply.github.com>
2024-08-13 22:51:51 +0530

    payment gateway

--- lib/views/cart_screen/cart_screen.dart
+++ lib/views/cart_screen/cart_screen.dart
@@ -1,18 +1,213 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
 
-class CartScreen extends StatelessWidget {
+class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
 
   @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  late Razorpay _razorpay;
+
+  void openCheckout(amount) async {
+    amount = amount * 100;
+    var options = {
+      'key': 'rzp_test_B93NMv5P48O7Ml',
+      'amount': amount,
+      'name': 'Test',
+      "prefil": {"contact": '8920800490', 'email': 'ishaanj2612@gmail.com'},
+      "external": {
+        "wallet": ["paytm"]
+      }
+    };
+    try {
+      _razorpay.open(options);
+    } catch (e) {
+      debugPrint('Error : e');
+    }
+  }
+
+  void handlePaymentSuccess(PaymentSuccessResponse response) {
+    Fluttertoast.showToast(
+        msg: "Payment Successful" + response.paymentId!,
+        toastLength: Toast.LENGTH_SHORT);
+  }
+
+  void handlePaymentError(PaymentFailureResponse response) {
+    Fluttertoast.showToast(
+        msg: "Payment Fail" + response.message!,
+        toastLength: Toast.LENGTH_SHORT);
+  }
+
+  void handleExternalWallet(ExternalWalletResponse response) {
+    Fluttertoast.showToast(
+        msg: "External Wallet" + response.walletName!,
+        toastLength: Toast.LENGTH_SHORT);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _razorpay.clear();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _razorpay = Razorpay();
+    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, handlePaymentSuccess);
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, handlePaymentError);
+    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, handleExternalWallet);
+  }
+
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shopping Cart'),
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
       ),
-      child: const Center(
-        child: Text(
-          'Cart is Empty!!',
-          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            // Cart items list
+            Expanded(
+              child: ListView.builder(
+                itemCount: 5, // Replace with your cart items length
+                itemBuilder: (context, index) {
+                  return CartItem(
+                    productImage: 'https://via.placeholder.com/150',
+                    productName: 'Product Name',
+                    productPrice: '\$19.99',
+                    quantity: 2,
+                  );
+                },
+              ),
+            ),
+            // Total and checkout button
+            Container(
+              padding: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                 const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Subtotal:',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        '\$99',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () => {
+                      setState(() {
+                        int amount = 99;
+                        openCheckout(amount);
+                      })
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.orange,
+                      padding: const EdgeInsets.all(15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: const Text('Checkout'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CartItem extends StatelessWidget {
+  final String productImage;
+  final String productName;
+  final String productPrice;
+  final int quantity;
+
+  const CartItem({
+    super.key,
+    required this.productImage,
+    required this.productName,
+    required this.productPrice,
+    required this.quantity,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          children: [
+            Image.network(
+              productImage,
+              width: 50,
+              height: 50,
+            ),
+            SizedBox(width: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  productName,
+                  style: TextStyle(fontSize: 16),
+                ),
+                Text(
+                  productPrice,
+                  style: TextStyle(fontSize: 14, color: Colors.grey),
+                ),
+              ],
+            ),
+            Spacer(),
+            Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.remove),
+                  onPressed: () => print('Remove item'),
+                ),
+                Text(
+                  '$quantity',
+                  style: TextStyle(fontSize: 16),
+                ),
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () => print('Add item'),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
